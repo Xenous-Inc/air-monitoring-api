85ef907798fc065ff01202c93997f1fc54916268
labamnus <labamnus@gmail.com>
2021-07-01 12:59:46 +0600

    Add air model

---
+++ src/models/Air.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const airSchema = new Schema({
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true,
+        },
+        coordinates: {
+            type: [Schema.Types.Number],
+            required: true,
+        },
+    },
+    sensorValue: { type: Schema.Types.String, required: true },
+    sensorType: { type: Schema.Types.String, required: true, enum: ['mq135'] },
+    date: {
+        type: Schema.Types.Date,
+        default: Date.now,
+        required: true,
+    },
+});
+
+export default mongoose.model('Air', airSchema);
